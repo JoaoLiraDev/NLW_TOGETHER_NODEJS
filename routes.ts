db5bc163adaad7261e52a324c5e6c13d797acb54
JoaoLiraDev <lirajoao414@gmail.com>
2021-06-23 16:53:55 -0300

    Third day in nlw

--- routes.ts
+++ routes.ts
@@ -1,10 +1,13 @@
 import express, { request, response, Router } from "express";
 import { CreateUsersController } from "./src/controllers/CreateUsersController";
-
+import { CreateTagController } from "./src/controllers/CreateTagController";
+import { ensureAdmin } from "./src/middlewares/ensureAdmin";
 const router = Router();
 
 const createUsersController = new CreateUsersController();
+const createTagController = new CreateTagController();
 
-router.post("/users", createUsersController.handle)
+router.post("/users", createUsersController.handle);
+router.post("/tags", ensureAdmin, createTagController.handle);
 
 export { router };
